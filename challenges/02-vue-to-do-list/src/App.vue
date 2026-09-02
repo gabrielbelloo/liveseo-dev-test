2542d68fa48e2630518bf76de8921b4a7d8feb32
Gabriel Bello <bellodealmeidagabriel@gmail.com>
2026-09-01 23:25:05 -0300

    feat: define todo task model

---
+++ challenges/02-vue-to-do-list/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface Task {
+  id: number
+  title: string
+  isCompleted: boolean
+}
+
+const tasks = ref<Task[]>([])
+
+</script>
+
+<template>
+</template>
+
+<style scoped></style>
