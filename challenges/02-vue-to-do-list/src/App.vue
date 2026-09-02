f98648ec4ee08a8b21870bed1f48562eba3f0ee8
Gabriel Bello <bellodealmeidagabriel@gmail.com>
2026-09-01 23:38:14 -0300

    feat: add todo filters

--- challenges/02-vue-to-do-list/src/App.vue
+++ challenges/02-vue-to-do-list/src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { ref, computed } from 'vue'
 import TodoForm from './components/TodoForm.vue'
+import TodoFilters from './components/TodoFilters.vue'
 import TodoItem from './components/TodoItem.vue'
 
 interface Task {
@@ -22,10 +24,26 @@ const removeTask = (taskId: number) => {
   tasks.value = tasks.value.filter(task => task.id !== taskId)
 }
 
+const filter = ref<'all' | 'pending' | 'completed'>('all')
+
+const filteredTasks = computed(() => {
+  if (filter.value === 'pending') {
+    return tasks.value.filter(task => !task.isCompleted)
+  }
+
+  if (filter.value === 'completed') {
+    return tasks.value.filter(task => task.isCompleted)
+  }
+
+  return tasks.value
+})
+
 </script>
 
 <template>
   <main>
+    <h1>To-do List</h1>
+    <TodoFilters @filter="filter = $event" />
     <TodoItem v-for="task in filteredTasks" :key="task.id" :task="task" @remove="removeTask" />
     <TodoForm @add="addTask" />
   </main>
---
+++ challenges/02-vue-to-do-list/src/components/TodoFilters.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+    const emit = defineEmits<{
+        filter: [filter: 'all' | 'pending' | 'completed']
+    }>()
+</script>
+
+<template>
+    <div>
+        <button @click="emit('filter', 'all')">Todas</button>
+        <button @click="emit('filter', 'pending')">Pendentes</button>
+        <button @click="emit('filter', 'completed')">Concluídas</button>
+    </div>
+</template>
+
+<style scoped></style>
