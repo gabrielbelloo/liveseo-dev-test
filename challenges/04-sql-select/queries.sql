698217ec686a49f8ab0c0958f199b9742e3de043
Gabriel Bello <bellodealmeidagabriel@gmail.com>
2026-09-03 13:01:37 -0300

    feat: add query to list users by creation date

--- challenges/04-sql-select/queries.sql
+++ challenges/04-sql-select/queries.sql
@@ -9,3 +9,4 @@ INSERT INTO users (name, email, created_at) VALUES ('Ana', 'ana@email.com', '202
   													('Pedro', 'pedro@email.com', '2024-02-01'), 
                                                     ('Maria', 'maria@email.com', '2024-02-15');
 
+SELECT * FROM users ORDER BY created_at DESC;
