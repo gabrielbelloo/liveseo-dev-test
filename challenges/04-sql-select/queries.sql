8a6aa311b814ddafb3a75739b8aecd8ac11c0478
Gabriel Bello <bellodealmeidagabriel@gmail.com>
2026-09-03 13:01:17 -0300

    feat: add users table and sample data

---
+++ challenges/04-sql-select/queries.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+	  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  	name TEXT,
+  	email TEXT,
+  	created_at DATE
+  );
+  
+INSERT INTO users (name, email, created_at) VALUES ('Ana', 'ana@email.com', '2024-01-01'), 
+  													('Pedro', 'pedro@email.com', '2024-02-01'), 
+                                                    ('Maria', 'maria@email.com', '2024-02-15');
+
